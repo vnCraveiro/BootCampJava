120ba8c9fa08a079384e4ee52775bffb1a44cf6f
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-06-23 13:13:21 -0300

    EXC 03 - DISCIPLICA 3 - AULA 3 - PARTE 6

--- src/Repeticao/Exc03_Disp3_Aula3_6.java
+++ src/Repeticao/Exc03_Disp3_Aula3_6.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class Exc03_Disp3_Aula3_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double base;
+        double altura;
+        double area;
+
+        System.out.print("Informe o tamanho da base em cm²: ");
+        base = sc.nextDouble();
+        System.out.print("Informe a altura do triangulo em cm²: ");
+        altura = sc.nextDouble();
+
+        while (base <=0 ) {
+            System.out.print("A base nao pode ser menor que 0cm², informe outro numero: ");
+            base = sc.nextDouble();
+            }
+        while (altura <= 0) {
+            System.out.print("A altura nao pode ser menor que 0cm², informe outro numero: ");
+            altura = sc.nextDouble();
+            }
+
+
+        area = (base*altura)/2;
+
+        System.out.printf("A Area de seu tringulo é de %.2f CM²",area);
 
 
 
