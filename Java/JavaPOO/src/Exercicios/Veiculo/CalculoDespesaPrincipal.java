f1a34706eaa5248f6c6baa76ba54e7670724452a
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-07-06 21:34:30 -0300

    Exercicio Veiculo Concluido

--- Java/JavaPOO/src/Exercicios/Veiculo/CalculoDespesaPrincipal.java
+++ Java/JavaPOO/src/Exercicios/Veiculo/CalculoDespesaPrincipal.java
@@ -1,7 +1,31 @@
-package JavaPOO.src.Exercicios.Veiculo;
+package Exercicios.Veiculo;
+
+import java.util.Scanner;
 
 public class CalculoDespesaPrincipal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int passageiro;
+        double tanque, consumo, km, valor;
+
+        System.out.println("Informe a quantidade de passageiros: ");
+        passageiro = sc.nextInt();
+        System.out.println("Capacidade do tanque do veículo? ");
+        tanque = sc.nextDouble();
+        System.out.println("Consumo por litro: ");
+        consumo = sc.nextDouble();
+        System.out.println("Distancia em KM: ");
+        km = sc.nextDouble();
+        System.out.println("Valor do combustível R$: ");
+        valor = sc.nextDouble();
+
+        Veiculo onibus = new Veiculo(passageiro, tanque, consumo);
+        System.out.printf(onibus.toString());
+
+        double tanques = onibus.tanqueViagem(km);
+        double rateio = onibus.dividirDespesas(km, valor);
+        System.out.printf("Para a viagem vamos precisar de %.1f tanque(s) de combustivel" +
+                "\nCada passageiro terá o custo de R$ %.2f ",tanques,rateio);
 
     }
 }
